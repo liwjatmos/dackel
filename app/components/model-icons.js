d03faf0ada0798f9358848bb8207dadd7d86acc7
Martin Fenner <martin.fenner@datacite.org>
2018-07-13 15:16:21 +0200

    show icons for filters

---
+++ app/components/model-icons.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['pull-right'],
+  certified: false,
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    this.set('open', this.get('model').get('dataAccesses').findBy('type', 'open'));
+    this.set('doi', this.get('model').get('pidSystems').findBy('text', 'DOI'));
+    this.set('urn', this.get('model').get('pidSystems').findBy('text', 'URN'));
+    this.set('ark', this.get('model').get('pidSystems').findBy('text', 'ARK'));
+    this.set('hdl', this.get('model').get('pidSystems').findBy('text', 'hdl'));
+    this.set('certified', this.get('model').get('certificates').length > 0);
+  }
+});
