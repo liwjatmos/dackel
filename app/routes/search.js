0acc0c006e1aab0404712e0145de6632de5897fc
Kristian Garza <kj.garza@gmail.com>
2019-01-21 17:19:19 +0100

    spaces

--- app/routes/search.js
+++ app/routes/search.js
@@ -4,7 +4,7 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 export default Ember.Route.extend(RouteMixin, {
   perPage: 50,
   collapsed: false,
-
+  
   model(params) {
     params.paramMapping = { page: "page[number]",
                             perPage: "page[size]",
